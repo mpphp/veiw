0df35ed8c905487130cc3d243404188af57bc5a7
tsommie <anitchec.dev@gmail.com>
2019-03-27 12:56:44 +0000

    fix typo.

---
+++ view.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @param string $template
+ * @param array $config
+ * @return string
+ */
+function view__resolve_directory(string $template, array $config):string
+{
+    $stripped_template = str_replace('.phtml', '', $template);
+
+    if (strpos($stripped_template, '::') > 0) {
+        $decoupled_template = explode(':', $stripped_template);
+
+        return $config[$decoupled_template[0]]['views'] . '/' . $decoupled_template[1] . '.phtml';
+    }
+
+    return $config['app']['paths']['views'] . '/' . $stripped_template . '.phtml';
+}
+
+/**
+ * @param string $template
+ */
+function view__extends(string $template)
+{
+    global $config;
+    require view__resolve_directory($template, $config);
+}
+
+/**
+ * @param string $template
+ * @return mixed
+ */
+function view__nests(string $template)
+{
+    return call_user_func($template);
+}
+
+/**
+ * @param string $template
+ * @param array $vars
+ */
+function view(string $template, array $vars = [])
+{
+    global $app;
+    extract($vars);
+    require view__resolve_directory($template, $app['config']);
+}
